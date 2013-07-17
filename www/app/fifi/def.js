b485fc1cb973673740f1fc16f6b128abf0e96ca9
jrburke <jrburke@gmail.com>
2013-07-16 20:53:44 -0700

    Update for wikipedia search change

--- www/app/fifi/def.js
+++ www/app/fifi/def.js
@@ -6,27 +6,15 @@ define(function (require) {
         var id = node.dataset.engineid;
         this.on['api/queryDone' + (id ? '/' + id : '')] = function (data) {
 
-
           // This assumes wikipedia data at the moment
-          var key,
-              query = data.result.query,
-              value = query && query.pages;
+          var value = data.result;
 
           this.data = {};
 
           if (value) {
-            key = Object.keys(value)[0];
-            if (key) {
-              value = value[key];
-              value = value.revisions &&
-                      value.revisions[0] &&
-                      value.revisions[0]['*'];
-              if (value) {
-                this.data = {
-                  html: value
-                };
-              }
-            }
+            this.data = {
+              html: value
+            };
           }
 
           this.render();
