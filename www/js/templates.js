c92b22c10f813b38e25dd709cfe891ca752ffbe8
Bryan Clark <clarkbw@gmail.com>
2013-08-08 16:48:16 -0700

    updating the template to the latest, got lost in the merge

--- www/js/templates.js
+++ www/js/templates.js
@@ -88,14 +88,9 @@ var lineno = null;
 var colno = null;
 var output = "";
 try {
-<<<<<<< HEAD
 output += "<div id=\"fifi-find-box\">\n\t<input type=\"text\" value=\"\" placeholder=\"Search...\" autocomplete=\"off\" id=\"fifi-find\">\n\t<div id=\"geolocation-box\">\n\t\t<button id=\"geolocation\" data-action=\"geolocation\"><i class=\"icon-location-arrow\"></i> <span id=\"geolocation-name\">Location</span></button>\n\t</div>\n</div>\n<div class=\"fifi-suggest\">\n  <ul class=\"suggestions\"></ul>\n</div>\n";
-return output;
-=======
-output += "<input type=\"search\" value=\"\" placeholder=\"find\" id=\"fifi-find\">\n<div class=\"fifi-suggest\">\n  <ul class=\"suggestions\"></ul>\n</div>\n";
 cb(null, output);
 ;
->>>>>>> 3c09eaf9ba34be14e2007c549581de3e63692c1e
 } catch (e) {
   cb(runtime.handleError(e, lineno, colno));
 }
